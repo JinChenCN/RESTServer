8174759f41a6c7ca46aea083b69bf3eb98437578
Jin Chen <chenjin10@gmail.com>
2015-10-12 14:03:53 +1300

    Changed project name

--- src/server/Main.java
+++ src/server/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Timer;
 
 import org.restlet.Component;
 import org.restlet.data.Protocol;
