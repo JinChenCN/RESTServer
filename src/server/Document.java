129084e7adc95aa061dd973dd51379a8479cb1a6
Jin Chen <chenjin10@gmail.com>
2015-10-18 13:36:22 +1300

    Return the file according to its mime type

--- src/server/Document.java
+++ src/server/Document.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.restlet.Request;
 import org.restlet.data.Form;
 import org.restlet.data.MediaType;
@@ -11,7 +15,7 @@ public class Document extends ServerResource {
 	String fileName = "";
 	
 	@Get
-    public FileRepresentation getResource() {
+    public FileRepresentation getResource() throws IOException {
 		FileRepresentation result = null;
 		Request request = getRequest();
 		Form form = request.getResourceRef().getQueryAsForm();
@@ -19,7 +23,39 @@ public class Document extends ServerResource {
 		{
 			fileName += form.getValues("fileName");
 		}		
-		result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.TEXT_HTML);
+		String fileType = Files.probeContentType(new File(Main.filePath + "//" + fileName).toPath());
+		if(fileType==null)
+		{
+			result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.TEXT_HTML);
+		}
+		else
+		{
+			switch (fileType) {
+	         case "text/plain":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.TEXT_HTML);
+	             break;
+	         case "image/jpeg":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.IMAGE_JPEG);
+	             break;
+	         case "image/png":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.IMAGE_PNG);
+	             break;
+	         case "application/pdf":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.APPLICATION_PDF);
+	        	 break;
+	         case "application/msword":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.APPLICATION_WORD);
+	             break;
+	         case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.APPLICATION_MSOFFICE_DOCX);
+	        	 break;
+	         case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.APPLICATION_MSOFFICE_XLSX);
+	        	 break;
+	         default:
+	        	 result = new FileRepresentation(Main.filePath + "//" + fileName, MediaType.TEXT_HTML);
+			}			
+		}		 
 		return result;
     }
 }
